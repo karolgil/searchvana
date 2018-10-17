dd75a6195fa33c553bf0dca5235f832c8cf0cd4b
Michał Bugno <michal@michalbugno.pl>
2018-10-17 20:29:06 +0200

    test

--- reader.rb
+++ reader.rb
@@ -3,7 +3,7 @@ require 'pp'
 require 'base64'
 
 COUNTER = {}
-FREQS = [" ", "e", "t", "a", "o", "i", "n", "s", "h", "r", "d", "l", "c", "u", "m", "w", "f", "g", "y", "p", "b", "v", "k", "j", "x", "q", "z"]
+FREQS = ["e", " ", "t", "a", "o", "i", "n", "s", "h", "r", "d", "l", "c", "u", "m", "w", "f", "g", "y", "p", "b", "v", "k", "j", "x", "q", "z"]
 
 def decode64(str)
   Base64.decode64(str)
@@ -16,6 +16,11 @@ def decrypt(str)
   str
 end
 
+def translate(s)
+  stats = {"ᚳ" => "t", "ᚧ" => "h", "ᚤ" => "e", "ᙟ" => " ", "ᚮ" => "o", "ᚱ" => "r", "ᚨ" => "i", "ᚠ" => "a", "ᚡ" => "b", "ᚫ" => "l", "ᚯ" => "p", "ᚬ" => "m", "ᚴ" => "u", "ᚩ" => "j", "ᚵ" => "v", "ᚲ" => "s", "ᚢ" => "c", "ᚪ" => "k", "ᚭ" => "n", "ᚸ" => "y", "ᚷ" => "x", "ᚶ" => "w", "ᚣ" => "d", "ᚥ" => "f", "ᚦ" => "g"}
+  stats[s]
+end
+
 TEXTS = []
 
 get '*' do
@@ -26,18 +31,20 @@ end
 post '/oMgogNpBPIYZZCelExnMvOsFXWBckXhhohKTDqYbweRkbmlLDw' do
   payload = JSON.parse(request.body.read)
   text = payload.fetch('text')
-  decrypt(text)
+  t = decrypt(text)
+  p t
+  t
 end
 
 post '/YgFIxFBaFQYoWsqIviNPLBhvEtYWYBLsjKZTCmYDksCKunMJOZ' do
   payload = JSON.parse(request.body.read)
-  p payload
+  # p payload
   # p request
   runes = payload.fetch('text')
-  p runes
-  p runes.bytes
+  # p runes
+  # p runes.bytes
   # runes.force_encoding('ISO-8859-1')
-  p runes
+  # p runes
   # p runes.codepoints
   # p runes.bytes
   chars = runes.split(//)
@@ -51,9 +58,18 @@ post '/YgFIxFBaFQYoWsqIviNPLBhvEtYWYBLsjKZTCmYDksCKunMJOZ' do
       '.'
     end
   end
-  p out
-  puts out.join('')
-  out
+
+  out2 = chars.map { |t| translate(t) || '.' }
+  puts out2.join('')
+  out2.join('')
+end
+
+post '/yLWRwqXJWoMcYEezqngieemuTpOpLoUoPNQnIRKRJfmOhLHBcE' do
+  payload = JSON.parse(request.body.read)
+  data = decode64(payload.fetch('text'))
+  p payload
+  p data
+  ''
 end
 
 put '*' do
